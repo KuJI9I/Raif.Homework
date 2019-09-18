31d9c0cf90ebe936f3462700d9194f4e21d7b210
NAYMUSHIN Igor <Igor.NAYMUSHIN@raiffeisen.ru>
2019-09-19 01:42:38 +0600

    Updated Task_4 Added Task_5

--- src/main/java/Task_4/Moon.java
+++ src/main/java/Task_4/Moon.java
@@ -11,7 +11,7 @@ public class Moon {
         System.out.println(check_2);
     }
 
-    static boolean check(String number) {
+    private static boolean check(String number) {
         int sum = 0;
         for (int i = 0; i <= number.length() - 1; i++) {
             int currentValue = Integer.parseInt(number.substring(i, i + 1));
---
+++ src/main/java/Task_5/Calculator.java
@@ -0,0 +1,86 @@
+package Task_5;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        while (true) {
+            int a = getInt();
+            char operation = getOperator();
+            int b = getInt();
+            int result = calculate(a, operation, b);
+            System.out.println("Получен результат: " + result);
+        }
+    }
+
+    private static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int num;
+//        if (scanner.hasNextInt()) {
+//            num = scanner.nextInt();
+//        } else {
+//            System.out.println("Некорректное значение числа. Попробуйте еще раз.");
+//            scanner.next();
+//            num = getInt();
+//        }
+        try {
+            num = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Некорректное значение числа. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    private static char getOperator() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите операцию: ");
+        char operator;
+        try {
+            operator = scanner.next().charAt(0);
+        } catch (Exception e) {
+            System.out.println("Некорректное значение операции. Попробуйте еще раз.");
+            scanner.next();
+            operator = getOperator();
+        }
+//        if (scanner.hasNext()) {
+//            operator = scanner.next().charAt(0);
+//        } else {
+//            System.out.println("Некорректное значение операции. Попробуйте еще раз.");
+//            scanner.next();
+//            operator = getOperator();
+//        }
+        return operator;
+    }
+
+
+    private static int calculate(int num1, char operation, int num2) {
+        int result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                try {
+                    result = num1 / num2;
+                } catch (Exception e) {
+                    System.out.println("Деление на ноль");
+                    result = num1 / getInt();
+                }
+
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calculate(num1, getOperator(), num2);
+        }
+        return result;
+    }
+}
